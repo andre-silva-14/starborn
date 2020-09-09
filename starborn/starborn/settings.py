01dff3d1df1f96709429e7cd17050bbb69147fe0
andre-silva-14 <silva.andre1997@gmail.com>
2020-09-09 19:44:02 +0200

    Remove Secret Key from settings
    - Secret key was moved from settings script into system environment
      for safety purposes. It has also been changed, for the same reason.

--- starborn/starborn/settings.py
+++ starborn/starborn/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q922ognk$zfj@pl)&+x+%45%3@emb@*)t=qudc5tj)nq+=!h$_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
