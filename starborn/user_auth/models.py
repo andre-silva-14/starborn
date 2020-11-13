d6c571e3a04a618d4cf551a6594e576c1eb41677
andre-silva-14 <silva.andre1997@gmail.com>
2020-11-13 21:30:57 +0100

    Change User Model back to default

--- starborn/user_auth/models.py
+++ starborn/user_auth/models.py
@@ -1,47 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self, email, username, password):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have an username')
-        if not password:
-            raise ValueError('Users must have a password')
-
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=46, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email',]
-
-    def __str__(self):
-        return self.username
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
+class User(AbstractUser):
+    pass
 
     
