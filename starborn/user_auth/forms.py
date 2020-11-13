c710a559a7290d55e3075fa945d05fff1579d653
andre-silva-14 <silva.andre1997@gmail.com>
2020-11-13 20:51:09 +0100

    Import necessary Class and remove redundant code

--- starborn/user_auth/forms.py
+++ starborn/user_auth/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.exceptions import ValidationError
 from .models import User
 
 
 class LoginForm(AuthenticationForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    # password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta():
-        fields = ('username',)
+        fields = ('username', 'password')
         model = User
 
     def __init__(self, *args, **kwargs):
