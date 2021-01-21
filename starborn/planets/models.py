05a29c54e5b60b70a5db05dbbf84e7b9b27c0b61
andre-silva-14 <silva.andre1997@gmail.com>
2021-01-21 19:39:27 +0100

    Integrate app with Planet models

--- starborn/planets/models.py
+++ starborn/planets/models.py
@@ -5,3 +5,12 @@ from django.db import models
 
 class Planet(models.Model):
     name = models.CharField(max_length=36)
+    inclination = models.DecimalField(decimal_places=3, max_digits=6)
+    radius = models.DecimalField(decimal_places=1, max_digits=20)
+    mass = models.CharField(max_length=100)
+    average_orbital_speed = models.DecimalField(decimal_places=3, max_digits=10)
+    orbital_period = models.DecimalField(decimal_places=9, max_digits=20)
+
+
+    def __str__(self):
+        return self.name
--- starborn/planets/views.py
+++ starborn/planets/views.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
-from django import template
+from .models import Planet
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -9,6 +10,6 @@ class PlanetView(TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['planet'] = kwargs.get('planet', None)
+        context['planet'] = get_object_or_404(Planet, name=kwargs.get('planet', None))
         context['planet_image'] = f"images/{context['planet']}.png"
         return context
